ac15a7129abc5b759dfa70c2eda84b24b34714db
Arka <arka.spectacular@gmail.com>
2021-08-18 10:41:17 +0530

    [Arka]Add:Added Gamble stake and bet

---
+++ GambleSimulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GambleSimulation
+{
+    class Program
+    {
+        public const int STAKE = 100;
+        public const int BET = 1;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to GambleSimulation!");
+        }
+    }
+}
